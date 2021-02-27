2954879a4a0ce9d25d544d3bf392f18d976facfc
Ankit Bhushan <66237346+AnkitBhushan788@users.noreply.github.com>
2021-02-27 11:29:23 +0530

    Add files via upload

---
+++ QRGenerate.java
@@ -0,0 +1,66 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+public class QRGenerate {
+
+    // Driver code
+    public static void main(String[] args) {
+
+        // The data that the QR code will contain
+//        String data = "www.geeksforgeeks.org";
+
+        String CMPCD="TTPLMT";
+        String LatLng="28.6609792::77.1482836";
+        String isLatLng="Y";
+
+
+
+        String PlayLink1="https://play.google.com/store/apps/details?cc=";
+        String PlayLink2="&id=com.translineindia.employeetracker&hl=en";
+
+//        String Data=CMPCD+"::"+isLatLng;  //for without latlng
+        String Data=CMPCD+"::"+isLatLng+"::"+LatLng;  //with latlng
+
+       String Enc= Base64.getEncoder().encodeToString(Data.getBytes(StandardCharsets.UTF_8));
+        // The path where the image will get saved
+
+        String QRData=PlayLink1+Enc+PlayLink2;
+        String path = "/home/ankit/QR/"+Data+".png";
+
+        // Encoding charset
+        String charset = "UTF-8";
+
+
+        createQR(QRData, path, charset, 500, 500);
+
+    }
+    public static void createQR(String data, String path,String charset,int height, int width){
+
+        try {
+            BitMatrix matrix = new MultiFormatWriter().encode(
+                    new String(data.getBytes(charset), charset),
+                    BarcodeFormat.QR_CODE, width, height);
+
+            MatrixToImageWriter.writeToFile(
+                    matrix,
+                    path.substring(path.lastIndexOf('.') + 1),
+                    new File(path));
+            System.out.println("QR Code Generated!!! ");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
